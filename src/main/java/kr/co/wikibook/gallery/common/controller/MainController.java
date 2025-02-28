0a2318bd25406b26d8bc46949552c4f402371854
9mans <rbska1234@naver.com>
2025-03-01 06:20:12 +0900

    쇼핑몰 토이프로젝트 완성

--- src/main/java/kr/co/wikibook/gallery/common/controller/MainController.java
+++ src/main/java/kr/co/wikibook/gallery/common/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping(value = {"/", "{path:[^.]*]}", "{path1:[^.]*}/{path2:[^.]*}"})
+    @GetMapping(value = {"/", "{path:[^.]*}", "{path1:[^.]*}/{path2:[^.]*}"})
     public String index() {
         return "/index.html";
 
